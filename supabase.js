38ac692a8f100bc8b0ad133efb87e77c46ace030
saraevansmusic557-stack <julieoscar175@gmail.com>
2026-09-06 07:19:55 -0500

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://yzvysiwqhlwfqqlstufb.supabase.co";
+const SUPABASE_KEY = "sb_publishable_kdCjQRNMTn5h9GnXStXROg_4uS90wYo";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
